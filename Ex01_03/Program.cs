13f8860c6c1d373744ecbdbbdff43101b702b0c5
rotemadimor <rotemadimor@gmail.com>
2025-04-21 13:38:28 +0300

    everything works except of the number of characters in each array

--- Ex01_03/Program.cs
+++ Ex01_03/Program.cs
@@ -11,15 +11,40 @@ namespace Ex01_03
     {
         public static void Main()
         {
-            int leafDigitToPrint = 0;
-            char FloorOfTree = 'A';
-            int[] digitsArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            Ex_02.Program.recursionFunc(digitsArray, leafDigitToPrint, FloorOfTree);
+            char FloorOfTree;
+            System.Console.WriteLine("Please enter a number for a tree floors, between 4-15.");
+            string inputFromUser = Console.ReadLine();
+            while (!checkInputValidation(inputFromUser, out FloorOfTree))
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 4-15.");
+                inputFromUser = Console.ReadLine();
+            }
+
+            Ex_02.Program.printTreeByFloor(FloorOfTree);
 
             Console.WriteLine("press any key to continue...");
             Console.ReadLine();
         }
 
-       
+        private static bool checkInputValidation(string inputFromUser, out char FloorOfTree)
+        {
+            FloorOfTree = ' ';
+            //int inputValue;
+            bool isInputValueValid = false;
+            bool isInputTypeValid = !string.IsNullOrEmpty(inputFromUser) && inputFromUser.Length == 1;
+            if (isInputTypeValid)
+            {
+                int inputValue = getInputValue(inputFromUser, out FloorOfTree);
+                isInputValueValid = inputValue >= 4 && inputValue <= 15;
+            }
+            return isInputValueValid;
+        }
+
+        private static int getInputValue(string inputFromUser, out char FloorOfTree)
+        {
+            int floorNumber = int.Parse(inputFromUser);
+            FloorOfTree = (char)(floorNumber + 'A');
+            return floorNumber;
+        }
     }
 }
--- Ex_02/Program.cs
+++ Ex_02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,54 +9,66 @@ namespace Ex_02
 {
     public class Program
     {
+        const int maxDigitIndex = 8;
+        const int minDigitIndex = 0;
+        const char space = ' ';
+        const string TreeLog = "|8|";
         public static void Main()
+        {
+            char maxFloor = 'G';
+            printTreeByFloor(maxFloor);
+
+            Console.WriteLine("press any key to continue...");
+            Console.ReadLine();
+        }
+        public static void printTreeByFloor(char maxFloor)
         {
             int leafDigitToPrint = 0;
             char FloorOfTree = 'A';
             int[] digitsArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            recursionFunc(digitsArray, leafDigitToPrint, FloorOfTree);
-
-            Console.WriteLine("press any key to continue...");
-            Console.ReadLine();
+            recursionFunc(digitsArray, leafDigitToPrint, FloorOfTree, maxFloor);
         }
 
-        public static void recursionFunc(int[] i_digitsArray, int i_nextLeafDigitToPrint, char i_FloorOfTree)
+        public static void recursionFunc(int[] i_digitsArray, int i_nextLeafDigitToPrint, char i_FloorOfTree, char maxFloor)
         {
+            int numberOfFloors = i_FloorOfTree - 'A';
             int digitsPrintedInThisFloor = 2 * (i_FloorOfTree - 'A') + 1;
-            string stringToPrint = GetDigitsToPrintByFloor(i_FloorOfTree, i_digitsArray, i_nextLeafDigitToPrint, digitsPrintedInThisFloor);
+            string stringToPrint = GetDigitsToPrintByFloor(i_FloorOfTree, i_digitsArray, i_nextLeafDigitToPrint, digitsPrintedInThisFloor, maxFloor);
             string formattedString1 = String.Format("{0}  {1}", i_FloorOfTree, stringToPrint);
             Console.WriteLine(formattedString1);
             i_FloorOfTree++;
-            if (i_FloorOfTree <= 'G')
+            if (i_FloorOfTree <= maxFloor)
             {
                 int nextDigit = GetNextDigitToPrintAsLeaf(i_nextLeafDigitToPrint, digitsPrintedInThisFloor);
-                recursionFunc(i_digitsArray, nextDigit, i_FloorOfTree);
+                recursionFunc(i_digitsArray, nextDigit, i_FloorOfTree, maxFloor);
             }
         }
-        public static int GetNextDigitToPrintAsLeaf(int i_nextLeafDigitToPrint, int digitsPrintedInThisFloor)
+        private static int GetNextDigitToPrintAsLeaf(int i_nextLeafDigitToPrint, int digitsPrintedInThisFloor)
         {
-            int nextDigit = i_nextLeafDigitToPrint + digitsPrintedInThisFloor;
-            if (nextDigit > 8)
+            int nextDigitIndex = i_nextLeafDigitToPrint + digitsPrintedInThisFloor;
+            if (nextDigitIndex > maxDigitIndex)
             {
-                nextDigit = 0;
+                nextDigitIndex = minDigitIndex;
             }
-            return nextDigit;
+            return nextDigitIndex;
         }
 
-        public static string GetDigitsToPrintByFloor(char floor, int[] io_digitsArray, int i_nextLeafDigitToPrint, int digitsPrintedInThisFloor)
+        private static string GetDigitsToPrintByFloor(char floor, int[] io_digitsArray, int i_nextLeafDigitToPrint, int digitsPrintedInThisFloor, char maxFloor)
         {
+            int maxCharactersInFloor = maxFloor - 'A' + 14; // TODO
+            char firstFloorOfTreeLog = (char)(maxFloor - 2);
             string stringToPrint = string.Empty;
-            char[] arrayToPrint = new string(' ', 20).ToCharArray();
+            char[] arrayToPrint = new string(space, maxCharactersInFloor).ToCharArray();
             int indexOffset = 2 * (floor - 'A') - 1;
             int startingPoint = (arrayToPrint.Length) / 2 - indexOffset;
-            if (floor <= 'E')
+            if (floor < firstFloorOfTreeLog)
             {
                 for (int i = 0; i < digitsPrintedInThisFloor; i++)
                 {
-                    if (i_nextLeafDigitToPrint > 8)
+                    if (i_nextLeafDigitToPrint > maxDigitIndex)
                     {
-                        i_nextLeafDigitToPrint = 0;
+                        i_nextLeafDigitToPrint = minDigitIndex;
                     }
                     arrayToPrint[startingPoint] = (char)(io_digitsArray[i_nextLeafDigitToPrint] + 48);
                     i_nextLeafDigitToPrint++;
@@ -64,10 +77,10 @@ namespace Ex_02
                 }
                 stringToPrint = new string(arrayToPrint);
             }
-            else if (floor <= 'G')
+            else if (floor < maxFloor)
             {
-                string rightSideSpaces = new string(' ', 10);
-                stringToPrint = string.Concat(rightSideSpaces, "|8|");
+                string rightSideSpaces = new string(space, 10);
+                stringToPrint = string.Concat(rightSideSpaces, TreeLog);
             }
             return stringToPrint;
         }
